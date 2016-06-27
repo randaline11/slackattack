68f70c3b015c3fadd705ad5990c3983cd794ba27
randaline11 <astarr2015@gmail.com>
2016-06-27 13:01:42 -0400

    added basic conversation functionality

--- app/server.js
+++ app/server.js
@@ -1,9 +1,19 @@
 import botkit from 'botkit';
 // this is es6 syntax for importing libraries
 // in older js this would be: var botkit = require('botkit')
+// import Yelp from 'yelp';
 
 // example bot
 console.log('starting bot');
+
+/*
+const yelp = new Yelp({
+  consumer_key: 'Et9qZ2uKjg6Tm3kEg3jMWA',
+  consumer_secret: '1cjQXnCi-0EneFYa5qhE3mbw4Rw',
+  token: '3CrQGbUI-Tq3QTcp-cTOLe8sgvOZsATr',
+  token_secret: '31Mh1StaVgUn4rgrdv5Ls9s9sh0',
+});
+*/
 
 // botkit controller
 const controller = botkit.slackbot({
@@ -39,3 +49,47 @@ controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 
     }
   });
 });
+
+controller.hears(['pizzatime'], ['direct_message', 'direct_mention'], (bot, message) => {
+  // askFlavor(response, convo);
+  bot.startConversation(message, askFlavor);
+//  bot.reply(message, 'pizzatime!');
+});
+
+
+/*
+controller.hears(['hello'], ['direct_message', 'direct_mention'], (bot, message) => {
+  // start a conversation to handle this response.
+  bot.startConversation(message, (err, convo) => {
+    convo.ask('How are you?', (response, convo2) => {
+      convo2.say('Cool, you said: ${response.text}');
+      convo2.next();
+      convo.next();
+    });
+  });
+});
+*/
+
+function askWhereDeliver(response, convo) {
+  convo.ask('So where do you want it delivered?', (response2, convo2) => {
+    convo2.say('Ok! Good bye.');
+  //  convo.next();
+    convo2.next();
+  });
+}
+function askSize(response, convo) {
+  convo.ask('What size do you want?', (response2, convo2) => {
+    convo2.say('Ok.');
+    askWhereDeliver(response2, convo2);
+//    convo.next();
+    convo2.next();
+  });
+}
+function askFlavor(response, convo) {
+  convo.ask('What flavor of pizza do you want?', (response2, convo2) => {
+    convo2.say('Awesome.');
+    askSize(response2, convo2);
+//    convo.next();
+    convo2.next();
+  });
+}
