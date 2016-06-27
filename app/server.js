2421ff4f280b739ad8683fff3b07bae8651d3562
randaline11 <astarr2015@gmail.com>
2016-06-27 18:31:00 -0400

    suggests top 3 results from Yelp, then bot self-destructs

--- app/server.js
+++ app/server.js
@@ -1,19 +1,14 @@
 import botkit from 'botkit';
 // this is es6 syntax for importing libraries
 // in older js this would be: var botkit = require('botkit')
-// import Yelp from 'yelp';
+import Yelp from 'yelp';
 
-// example bot
-console.log('starting bot');
-
-/*
 const yelp = new Yelp({
   consumer_key: 'Et9qZ2uKjg6Tm3kEg3jMWA',
   consumer_secret: '1cjQXnCi-0EneFYa5qhE3mbw4Rw',
   token: '3CrQGbUI-Tq3QTcp-cTOLe8sgvOZsATr',
   token_secret: '31Mh1StaVgUn4rgrdv5Ls9s9sh0',
 });
-*/
 
 // botkit controller
 const controller = botkit.slackbot({
@@ -39,8 +34,6 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
 
 // example hello response
 controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-//  bot.reply(message, 'Hello there!');
-
   bot.api.users.info({ user: message.user }, (err, res) => {
     if (res) {
       bot.reply(message, `Hello, ${res.user.name}!`);
@@ -50,46 +43,67 @@ controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 
   });
 });
 
-controller.hears(['pizzatime'], ['direct_message', 'direct_mention'], (bot, message) => {
-  // askFlavor(response, convo);
-  bot.startConversation(message, askFlavor);
-//  bot.reply(message, 'pizzatime!');
+controller.hears(['hungry'], ['direct_message', 'direct_mention'], (bot, message) => {
+  bot.startConversation(message, askTerm);
 });
 
-
-/*
-controller.hears(['hello'], ['direct_message', 'direct_mention'], (bot, message) => {
-  // start a conversation to handle this response.
-  bot.startConversation(message, (err, convo) => {
-    convo.ask('How are you?', (response, convo2) => {
-      convo2.say('Cool, you said: ${response.text}');
-      convo2.next();
-      convo.next();
-    });
+function respond(name, phone, url, destruct) {
+  controller.on('user_typing', (bot, message) => {
+    if (destruct) {
+      bot.reply(message, 'self-destructing now....');
+      setTimeout(bot.destroy.bind(bot), 1000);
+    } else
+    {
+      bot.reply(message, name);
+      bot.reply(message, phone);
+      bot.reply(message, url);
+    }
   });
-});
-*/
+}
 
 function askWhereDeliver(response, convo) {
   convo.ask('So where do you want it delivered?', (response2, convo2) => {
-    convo2.say('Ok! Good bye.');
-  //  convo.next();
+    convo2.say('Ok! Good bye. type ready for your food recommendations!');
     convo2.next();
-  });
-}
-function askSize(response, convo) {
-  convo.ask('What size do you want?', (response2, convo2) => {
+
+    // handle conversation end and extract responses
+    convo2.on('end', (convo3) => {
+      if (convo3.status === 'completed') {
+        const term = convo3.extractResponse('term');
+        const miles = (parseInt(convo3.extractResponse('miles'), 10) * 1609.34);
+        const location = convo3.extractResponse('location');
+
+        // integrate Yelp search
+        yelp.search({ term, location, radius_filter: miles, limit: 3 })
+        .then((data) => {
+          data.businesses.forEach(business => {
+            console.log(business.name);
+            respond(business.name, business.phone, business.url, false);
+          });
+          respond('', '', '', true);
+        })
+          .catch((err) => {
+            console.error(err);
+          });
+      } else {
+        console.log('didnt complete');
+      }  // else
+    });  // convo2.on end
+  }, { key: 'location' });  // convo.ask
+}  // askWhereDeliver
+
+function askMiles(response, convo) {
+  convo.ask('how far are you willing to go in miles?', (response2, convo2) => {
     convo2.say('Ok.');
     askWhereDeliver(response2, convo2);
-//    convo.next();
     convo2.next();
-  });
+  }, { key: 'miles' });
 }
-function askFlavor(response, convo) {
-  convo.ask('What flavor of pizza do you want?', (response2, convo2) => {
+
+function askTerm(response, convo) {
+  convo.ask('What kind of food do you want?', (response2, convo2) => {
     convo2.say('Awesome.');
-    askSize(response2, convo2);
-//    convo.next();
+    askMiles(response2, convo2);
     convo2.next();
-  });
+  }, { key: 'term' });
 }
