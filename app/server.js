3e210e6c540a67bfc5acb189e0ecc891333bcd84
randaline11 <astarr2015@gmail.com>
2016-07-03 13:19:24 -0400

    big code cleanup, rearranging, commenting, and new error logging function

--- app/server.js
+++ app/server.js
@@ -1,14 +1,16 @@
+/*
+Mr-Tron: a slackbot that tracks Github commits in channels
+made by Abby Starr and Tim Tregubov
+last revised: 7/3/2016
+Uses Slack's bot user API and Google Spreadsheets API
+Commented code for Github's API left in for potential future variations.
+*/
 import botkit from 'botkit';
-import Github from 'github-api';
 import moment from 'moment';
-
-// Note: There are better APIs out there, but not many for Node and all use
-// google's v3 API version, which will eventually be deprecated. Therefore,
-// direct google-spreadsheet is the better choice.
 import GoogleSpreadsheet from 'google-spreadsheet';
-// taken from Dali Lb's hr-bot
+// import Github from 'github-api';  //Github API
 
-
+// Authorization with Google
 const googleCreds = {
   type: process.env.TYPE,
   project_id: process.env.PROJECT_ID,
@@ -22,8 +24,31 @@ const googleCreds = {
   client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
 };
 
+// Github API
+/*
+const gh = new Github({
+  username: 'randaline11',
+  password: process.env.GITHUB_PASSWORD,
+});
+*/
+
+// botkit controller
+const controller = botkit.slackbot({
+  debug: false,
+});
+
+// initialize slackbot
+const slackbot = controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN,
+}).startRTM(err => {
+  // start the real time message client
+  if (err) { throw new Error(err); }
+});
+
+// initialize desired spreadsheet
 const spreadsheet = new GoogleSpreadsheet(process.env.SPREADSHEET);
 
+// all functions related to spreadsheet API
 const Spreadsheets = {
 
   // authorizes and signs in google account app credentials
@@ -31,7 +56,6 @@ const Spreadsheets = {
     return new Promise((fulfill, reject) => {
       console.log('getAuth');
       spreadsheet.useServiceAccountAuth((googleCreds), (err) => {
-        console.log('got into the function?');
         if (err) {
           reject(err);
           console.log('the getAuth promise was rejected....');
@@ -44,9 +68,11 @@ const Spreadsheets = {
     });
   },
 
-  // gets specific sheet from spreadsheet
+  // gets specific worksheet from spreadsheet based on channel name
+  // spreadSheetName: the name of the desired worksheet
+  // modeled after DALI Lab's HR bot
   getSpreadSheet(spreadsheetName) {
-    let sheet;
+    let sheet;  // the desired worksheet
     console.log(`getSpreadSheet: ${spreadsheetName}`);
     return new Promise((fulfill, reject) => {
       spreadsheet.getInfo((err, sheetInfo) => {
@@ -64,13 +90,18 @@ const Spreadsheets = {
           } else {
             reject(new Error(`couldn't find spreadsheet: ${spreadsheetName}`));
           }
-        }
-      });
-    });
+        }  // else
+      });  // spreadsheet.getInfo API call
+    });  // promise
   },
 
-  // adds a new row with the commit info
+  // adds a new row to the worksheet with the commit info
+  // sheet: the worksheet
+  // username: username of person who made commit
+  // text: commit message
+  // url: link to the commit
   addInfo(sheet, username, text, url) {
+    console.log('adding commit to the worksheet...');
     return new Promise((fulfill, reject) => {
       const now = moment();
       sheet.addRow({ date: now, username, text, url }, (err, row) => {
@@ -78,6 +109,7 @@ const Spreadsheets = {
           fulfill(row);
         } else {
           reject(err);
+          logErrors(err, 'addInfo');
         }
       });  // sheet
     });  // promise
@@ -85,48 +117,12 @@ const Spreadsheets = {
 
 };  // Spreadsheets object
 
-const gh = new Github({
-  username: 'randaline11',
-  password: process.env.GITHUB_PASSWORD,
-});
-
-// botkit controller
-const controller = botkit.slackbot({
-  debug: false,
-});
-
-// initialize slackbot
-const slackbot = controller.spawn({
-  token: process.env.SLACK_BOT_TOKEN,
-
-  // this grabs the slack token we exported earlier
-}).startRTM(err => {
-  // start the real time message client
-  if (err) { throw new Error(err); }
-});
-
-// prepare webhook
-// for now we won't use this but feel free to look up slack webhooks
-controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
-  controller.createWebhookEndpoints(webserver, slackbot, () => {
-    if (err) { throw new Error(err); }
-  });
-});
-
-// example hello response
-controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention', 'ambient'], (bot, message) => {
-  bot.api.users.info({ user: message.user }, (err, res) => {
-    if (res) {
-      console.log(process.env.SPREADSHEET);
-
-      bot.reply(message, `Hello, ${res.user.name}!`);
-    } else {
-      bot.reply(message, 'Hello there!');
-    }
-  });
-});
-
+// All functions related to Slack API
 const BotStuff = {
+
+  // gets the channel name.
+  // bot: the message bot object created with botkit
+  // message: object returned from an event
   getChannel(bot, message) {
     console.log('getting channel name');
     return new Promise((fulfill, reject) => {
@@ -137,11 +133,14 @@ const BotStuff = {
           fulfill(res.channel.name);
         } else {
           reject(err);
+          logErrors(err, 'getChannel');
         }
       });  // channel name
     });  // promise
   },
 
+  // gets everything from channel needed for input into worksheet
+  // message: object returned from an event
   getOtherVars(message) {
     console.log('in getOtherVars');
     const messageText = message.attachments[0].text.split(' ');
@@ -153,15 +152,33 @@ const BotStuff = {
     return completedVars;
   },
 
-};  // the big one
+};  // end of BotStuff object
+
+// uses an extra spreadsheet to keep track of all errors
+// err: the error message from rejected promise
+// functionName: the function the error message came from
+function logErrors(err, functionName) {
+  console.log('there was an error somewhere. adding error to log worksheet');
+  const now = moment();
+  Spreadsheets.getSpreadSheet('errors').then((sheet) => {
+    sheet.addRow({ date: now, functionName, err }, (err, row) => {
+      if (row) {
+        console.log('logged error in errors worksheet');
+      } else {
+        console.log(`'failed to log error. was: ${err} in ${functionName}`);
+      }
+    });  // sheet.addRow API call
+  });  // getSpreadsheet
+}
 
 // checking all messages now
 controller.on(['bot_message'], (bot, message) => {
   console.log(`bot id ${message.bot_id}`);
     // confirm that the message came from github bot
-  if (message.bot_id === 'B1M5QB84F') {
+  if (message.bot_id === 'B1M5QB84F') {  // ID belongs to Github Bot
     bot.reply(message, 'Github bot, is that you?');
 
+    // then start main promise chain
     Spreadsheets.getAuth().then((name) => {
       return BotStuff.getChannel(bot, message);
     })
@@ -173,50 +190,33 @@ controller.on(['bot_message'], (bot, message) => {
       return Spreadsheets.addInfo(sheet, addThis[0], addThis[1], addThis[2]);
     })
     .then((row) => {
-      console.log(`youre done! ${row}`);
+      console.log('done!');
     });
 
+    // commented out code for use with Github API
     /*
     const fallback = message.attachments[0].fallback;
     console.log('fallback: ' + fallback);
-
     const processFallback = fallback.split(' ');
     const commitName = processFallback[processFallback.length - 1];  // person who made commitName
     const url = processFallback[0].split('/');  // url of repo in array form
     console.log('url is ' + url);
     const owner = url[3];
     const repo = url[4];
-    console.log('owner is: ' + owner);
-    console.log('repo is ' + repo);
-
-*/
-    // getting sha for github
-    /*
-
     const processMore = processFallback[1].split('|')[0].split('/');
     const sha = processMore[(processMore.length - 1)];
     const tempSha = '70ce05550b90f7872e84fcc99ad0346201b74a2e';
     console.log( 'sha: ' + sha);
-
     const commitCheck = gh.getRepo(owner, repo);
     console.log(commitCheck);
     commitCheck.getCommit(tempSha, (err, res) => {
       if (res) {
         console.log(res);
-        console.log('____________');
-        console.log(res.author);
-        console.log('____________');
-        console.log(res.stats);
       }
       else {
         console.log(err);
       }
-
     });
-    */
-
-    // now query github to get more cool info
-    /*
     const commitInfo = gh.getCommit(sha, (err, res) => {
       if (res) {
         console.log(res);
@@ -230,10 +230,14 @@ controller.on(['bot_message'], (bot, message) => {
   }
 });
 
-
-controller.on('ambient', (bot, message) => {
-  console.log(message);
-  bot.reply(message, {
-    text: 'A more complex response',
+// example hello response -- taken from slackattack guide
+controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention',
+'mention', 'ambient'], (bot, message) => {
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    if (res) {
+      bot.reply(message, `Hello, ${res.user.name}!`);
+    } else {
+      bot.reply(message, 'Hello there!');
+    }
   });
 });
